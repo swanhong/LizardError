f8686544a3b53f3348737b0f7fcd09a66afaec61
swanhong <swan_hong@naver.com>
2017-11-28 11:46:08 +0900

    Initial files

---
+++ main.py
@@ -0,0 +1,16 @@
+from Lizard import runLizardError
+from RLizard import runRLizardError
+
+
+runLizardError(category=1, n=536)
+runLizardError(category=1, n=663)
+runLizardError(category=3, n=816)
+runLizardError(category=3, n=952)
+runLizardError(category=5, n=1088)
+runLizardError(category=5, n=1300)
+
+
+runRLizardError(category=1, n=1024)
+runRLizardError(category=3, n=1024)
+runRLizardError(category=3, n=2048)
+runRLizardError(category=5, n=2048)
